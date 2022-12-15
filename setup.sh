354842d40c336bb75cf7cbeede29acdb341f3347
ivan.heibi <ivanhb.ita@gmail.com>
2022-12-15 16:37:43 +0100

    sh install for py3.9

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd src/python/
+echo "Extracting python3.9 ..."
+tar xvf Python-3.9.0.tgz
+echo "Installing python3.9 locally ..."
+cd Python-3.9.0
+./configure
+make
+echo "Building a virtual environment ..."
+python -m venv ../mitaovenv_py3_9
+echo "Activating the virtual environment"
+cd ../
+source mitaovenv_py3_9/bin/activate
+cd ../../
+echo "Installing the requirements ..."
+pip install -r requirements.txt
+echo "MITAO has been successfully installed !!"
+deactivate
